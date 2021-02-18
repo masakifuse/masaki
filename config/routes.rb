28989557cebb012c78fd67e8eca8e5cd32e1f374
masakifuse <masaki.fuse1021@gmail.com>
2021-02-19 05:58:23 +0900

    masaki

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  # get 'users/index'
+  # get 'users/show'
+  # get 'users/edit'
+  # get 'home/index'消したいらないみたい
+  devise_for :users
   root to: "home#index"
+  resources :users
 end
